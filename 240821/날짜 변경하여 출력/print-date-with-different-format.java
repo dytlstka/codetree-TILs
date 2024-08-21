da320f17a2c0d13be30a063185c3135ec86ee8a0
dytlstka <103085051+dytlstka@users.noreply.github.com>
2024-08-21 22:32:38 +0900

    Title: 날짜 변경하여 출력, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240821/날짜 변경하여 출력/print-date-with-different-format.java
+++ 240821/날짜 변경하여 출력/print-date-with-different-format.java
@@ -6,7 +6,7 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        sc.useDelimiter(".");
+        sc.useDelimiter("\\.");
 
         int a = sc.nextInt();
         int b = sc.nextInt();
