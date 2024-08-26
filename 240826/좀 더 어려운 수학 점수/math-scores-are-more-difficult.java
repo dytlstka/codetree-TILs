cfd4bb807189b9399b5c3823c5355c8c2ae707c1
dytlstka <103085051+dytlstka@users.noreply.github.com>
2024-08-26 16:07:58 +0900

    Title: 좀 더 어려운 수학 점수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240826/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        
+
+        if (a>c){
+            System.out.print("B");
+        }
+        else (a<c){
+            System.out.print("A");
+        }
+
+        if (b>d){
+            System.out.print("B");
+        }
+        else (b<d){
+            System.out.print("A");
+        }
+
+
+    }
+}
