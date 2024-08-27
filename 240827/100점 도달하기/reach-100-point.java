e4af233f53100049e020055d28d2889b585884af
dytlstka <103085051+dytlstka@users.noreply.github.com>
2024-08-27 17:13:01 +0900

    Title: 100점 도달하기, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240827/100점 도달하기/reach-100-point.java
+++ 240827/100점 도달하기/reach-100-point.java
@@ -9,22 +9,18 @@ public class Main {
         int n = sc.nextInt();
         
 
-        for (int i = 1; i <= 100; i++){
+        for (int i = n; i <= 100; i++){
             
 
             if(n>=90){
                 System.out.print("A ");
-
             }            
             else if(n>=80){
                 System.out.print("B ");
             }
-            
             else if(n>=70){
                 System.out.print("C ");
             }
-
-
             else if(n>=60){
                 System.out.print("D ");
             }
