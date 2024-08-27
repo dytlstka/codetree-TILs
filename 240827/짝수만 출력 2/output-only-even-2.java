acdf8ad0ac084e91c1dbf5a995d207d2cb0d508b
dytlstka <103085051+dytlstka@users.noreply.github.com>
2024-08-27 13:45:19 +0900

    Title: 짝수만 출력 2, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240827/짝수만 출력 2/output-only-even-2.java
+++ 240827/짝수만 출력 2/output-only-even-2.java
@@ -6,8 +6,8 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
         int b = sc.nextInt();
+        int a = sc.nextInt();
 
         while ( b >= a){
 
