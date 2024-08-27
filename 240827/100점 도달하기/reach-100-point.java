4d589c65a74a8400dc56f76a8b22d896c0142a42
dytlstka <103085051+dytlstka@users.noreply.github.com>
2024-08-27 17:04:22 +0900

    Title: 100점 도달하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240827/100점 도달하기/reach-100-point.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+
+        for (int i = 1; i <= 100; i++){
+            if(n>=80){
+                System.out.print("B ")
+            }
+            else if(n>=90){
+                System.out.print("A ")
+            }
+        }
+        
+        
+    }
+}
