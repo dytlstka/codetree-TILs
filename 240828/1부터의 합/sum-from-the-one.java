cd2fd500d3840273b04be7447a57ad3971e3e399
dytlstka <103085051+dytlstka@users.noreply.github.com>
2024-08-28 17:47:48 +0900

    Title: 1부터의 합, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 240828/1부터의 합/sum-from-the-one.java
+++ 240828/1부터의 합/sum-from-the-one.java
@@ -4,20 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        // 변수 선언 및 입력
+        // 변수 선언
         int n;
         int sum = 0;
+
         n = sc.nextInt();
         
-        
+        // 1부터 증가시키며 더한 값이 n이상이 된 순간, 더해진 숫자를 출력합니다.
         for(int i = 1; i <= 100; i++) {
-            if(sum>n){
+            sum += i;
+            if(sum >= n) {
+                System.out.print(i);
                 break;
             }
-            sum+=i;
-        
         }
-
-        System.out.print(sum);
     }
 }
