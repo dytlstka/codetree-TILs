03dd048862aa9738374b12c3014ca2439420b9b0
dytlstka <103085051+dytlstka@users.noreply.github.com>
2024-08-28 16:49:00 +0900

    Title: 완전수 판별기, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240828/완전수 판별기/perfect-number-discriminator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int t = 0;
+
+        for(int i =  1; i <= n; i++){
+            if(n%i==0){
+                t+=i;
+            }
+            
+        }
+        if(n == (t-n)){
+            System.out.print("P");
+        }
+        else
+            System.out.print("N");
+        
+    }
+}
