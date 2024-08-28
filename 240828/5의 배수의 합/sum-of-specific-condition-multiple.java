7f0811d2fceebdb7bd9ba9a4ef3c5cb5a78c9919
dytlstka <103085051+dytlstka@users.noreply.github.com>
2024-08-28 16:28:42 +0900

    Title: 5의 배수의 합, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240828/5의 배수의 합/sum-of-specific-condition-multiple.java
+++ 240828/5의 배수의 합/sum-of-specific-condition-multiple.java
@@ -2,30 +2,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-
         Scanner sc = new Scanner(System.in);
+        
+        // 변수 선언 및 입력
+        int a, b;
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int cnt = 0;
-        if(a>b){
+        a = sc.nextInt();
+        b = sc.nextInt();
 
-            for(int i = b; b<=a; i++){
-                if(i%5==0){
-                    cnt+=i;
-                }
+        int sum = 0;
+
+        // Case 1. b가 더 큰 경우 
+        if(a <= b) {
+            for(int i = a; i <= b; i++) {
+                if(i % 5 == 0)
+                    sum += i;
             }
         }
-        else
-            {
-                for(int i = a; a<=b; i++){
-                    if(i%5==0){
-                        cnt+=i;
-                    }
-                }
+        // Case 2. a가 더 큰 경우
+        else {
+            for(int i = b; i <= a; i++) {
+                if(i % 5 == 0)
+                    sum += i;
             }
-        
-        System.out.print(cnt);
+        }
+    
+        // 출력
+        System.out.println(sum);
     }
 }
