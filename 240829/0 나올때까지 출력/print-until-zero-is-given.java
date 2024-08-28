e9df76ae25c91da18ca406647dce7672c063b663
dytlstka <103085051+dytlstka@users.noreply.github.com>
2024-08-29 01:39:05 +0900

    Title: 0 나올때까지 출력, Time: 199ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240829/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+
+        while(true){
+            int k = sc.nextInt();
+                if( k == 0){
+                    break;
+
+                }
+                System.out.println(k);
+
+        }
+    }
+}
