f94167ce225038308eda09fe233b6b0cd51f6abc
dytlstka <103085051+dytlstka@users.noreply.github.com>
2024-08-28 17:56:22 +0900

    Title: 1부터 곱셈하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240828/1부터 곱셈하기/multiple-from-one.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 변수 선언
+        int n;
+        int sum = 1;
+
+        n = sc.nextInt();
+        
+        // 1부터 증가시키며 더한 값이 n이상이 된 순간, 더해진 숫자를 출력합니다.
+        for(int i = 1; i <= 10; i++) {
+            sum *= i;
+            if(sum >= n) {
+                System.out.print(i);
+                break;
+            }
+        }
+    }
+}
