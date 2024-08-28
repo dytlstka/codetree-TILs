f401c096733dd42bd265886986a7b917d5313487
dytlstka <103085051+dytlstka@users.noreply.github.com>
2024-08-28 17:06:13 +0900

    Title: a의 b승, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240828/a의 b승/a-to-the-power-of-b.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int m = 1;
+
+        for (int i = 1; i<=b; i++){
+            m *= a;
+        }
+        
+        System.out.print(m);
+    }
+}
