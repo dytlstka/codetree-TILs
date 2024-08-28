6d4334a5842c377b5b0d5ae8bb078c0d599ec930
dytlstka <103085051+dytlstka@users.noreply.github.com>
2024-08-28 15:46:25 +0900

    Title: 등장횟수 확인, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240828/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+        for (int i = 1; i<=5; i++){
+            int p = sc.nextInt();
+            if(p%2==0){
+                cnt++;
+            }
+            
+        }
+        System.out.print(cnt);
+        
+        
+    }
+}
