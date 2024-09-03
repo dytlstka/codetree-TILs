d6174429e02c9b368aa1aea33c4deeba690ce863
dytlstka <103085051+dytlstka@users.noreply.github.com>
2024-09-03 14:41:31 +0900

    Title: 나눗셈의 나머지, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240903/나눗셈의 나머지/remainder-of-division.java
+++ 240903/나눗셈의 나머지/remainder-of-division.java
@@ -9,12 +9,30 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
 
-        int a = sc.nextInt();
+        int p = sc.nextInt();
         int b = sc.nextInt();
+
+        int a = p;
         int sum = 0;
-        int[] Arr = new int[b+1];
-        int[] CountArr = new int[b];
         
+        int[] CountArr = new int[b];
+
+        int take = 0;
+
+        while(true){
+            
+            if(p<b){
+                p = p/b;
+                take++;
+                break;
+            }
+            else{
+                p = p/b;
+                take++;
+            }
+
+        }
+        int[] Arr = new int[take];
 
         for(int i = 0; i<b+1; i++){
 
